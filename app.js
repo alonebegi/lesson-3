254ed79c26c227bd3277cf0f4cc906b879071b33
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-04-02 13:01:19 +0300

    lesson 4 function

---
+++ app.js
@@ -0,0 +1,42 @@
+
+// 1masala
+// function soat(a){
+// a = +prompt("soat kriting: ");
+// let b = 60 * a;
+// console.log(b);
+// }
+// soat();
+
+
+// 2masala
+// function addition(a){
+//     a = +prompt("sonni kriting: ");
+//     a++
+//     console.log(a);
+//     return a;
+// }
+// addition();
+
+// 3masala
+// function giveSomething(a){
+//     a = prompt("gap yozing unga something qoshiladi: ");
+// let b = "something";
+// let c = (b + " " + a );
+// console.log(c); 
+// }
+// giveSomething();
+
+// masala 5
+// function yilSanaKUN(yil, oy, kun){
+//     yil = +prompt("yil kriting");
+//     oy = +prompt("oy kriting");
+//     kun = +prompt("kun kriting");
+//     let sana = 2017;
+//     let anqOY= oy * 30;
+//     let AnqYil = yil - sana;
+//     let yilik = AnqYil * 365;
+//     let hamasi = yilik + anqOY + kun;
+//     console.log(hamasi);
+//     return hamasi;
+// }
+// yilSanaKUN()
